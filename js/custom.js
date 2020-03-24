7498c75e851c572278f07443ac1ffebf8506ea8f
Vernon Johnson <vtj2105@columbia.edu>
2020-03-23 23:20:16 -0400

    fix: email form submission

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,25 @@
 $( document ).ready(function() {
-    const form = document.getElementById('emailForm');
+  const form = document.getElementById('emailForm');
+  if (form) {
     form.addEventListener('submit', sendMobileEmail);
-    const form2 = document.getElementById('emailForm2');
+  }
+
+  const form2 = document.getElementById('emailForm2');
+
+  if (form2){
     form2.addEventListener('submit', sendMobileEmail);
+  }
 });
 
 
 
 function sendMobileEmail (event) {
   event.preventDefault()
-
   let email
 
   const email1 = $('#email55').val()
   const email2 = $('#email56').val()
+  console.log(email1, email2)
 
   if (email1 && email1.length > 0){
     email = email1
