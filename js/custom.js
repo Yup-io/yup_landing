d559064524b1f17197f22aa6cea0238a7f85a8bc
pinkhoodie <36429880+pinkhoodie@users.noreply.github.com>
2020-12-11 03:10:43 -0500

    center and animations onscroll

--- js/custom.js
+++ js/custom.js
@@ -180,3 +180,8 @@ $(document).ready(async function() {
     return formattedMetric
   }
 })
+
+
+window.addEventListener('scroll', () => {
+  document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+}, false);
